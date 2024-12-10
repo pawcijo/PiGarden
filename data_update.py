3043032b1b971d6e577f4bc6391a50e1f77caa52
pawcijo <pawcijo@gmail.com>
2024-12-11 00:43:33 +0100

    remove duplicated code

--- data_update.py
+++ data_update.py
@@ -2,27 +2,21 @@ import smbus
 import time
 from datetime import datetime
 import sqlite3
-from sensor_utils import read_soil_moisture, get_cpu_temperature
 import pytz
+from sensor_utils import read_soil_moisture, get_cpu_temperature, read_temperature_humidity
 
-ADC_ADDRESS = 0x48  # I2C address for ADC
-SHT31_ADDRESS = 0x44  # I2C address for SHT31-D sensor
+# Constants
 SLEEP_INTERVAL = 3600  # Interval to push data to the database (in seconds)
 
 # Function to push data into the database
 def push_data():
     while True:
         try:
-            bus = smbus.SMBus(1)
-            # Read temperature and humidity data from SHT31 sensor
-            bus.write_i2c_block_data(SHT31_ADDRESS, 0x2C, [0x06])
-            time.sleep(0.5)
-            data = bus.read_i2c_block_data(SHT31_ADDRESS, 0x00, 6)
+            # Read temperature and humidity from SHT31 sensor
+            cTemp, humidity = read_temperature_humidity()
 
-            if len(data) == 6:
-                temp = data[0] * 256 + data[1]
-                cTemp = -45 + (175 * temp / 65535.0)
-                humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+            if cTemp is not None and humidity is not None:
+                # Read other sensor values
                 soil_moisture = read_soil_moisture(0)  # Using channel 0 for soil moisture
                 cpu_temperature = get_cpu_temperature()
                 local_time = datetime.now(pytz.timezone("Europe/Warsaw")).strftime("%Y-%m-%d %H:%M:%S")
@@ -30,10 +24,13 @@ def push_data():
                 # Store data in the database
                 conn = sqlite3.connect("sensor_data.db")
                 cursor = conn.cursor()
-                cursor.execute("""
+                cursor.execute(
+                    """
                     INSERT INTO sensor_readings (timestamp, temperature, humidity, soil_moisture, cpu_temperature)
                     VALUES (?, ?, ?, ?, ?)
-                """, (local_time, cTemp, humidity, soil_moisture, cpu_temperature))
+                    """,
+                    (local_time, cTemp, humidity, soil_moisture, cpu_temperature),
+                )
                 conn.commit()
                 conn.close()
 
